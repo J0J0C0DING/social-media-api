7a387dd3f1f33a7fa40042665532e727fb00eaac
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-08-26 00:17:57 -0500

    user model updated

--- models/User.js
+++ models/User.js
@@ -1,3 +1,52 @@
 const { Schema, model } = require('mongoose');
 
-import dateFormat, { masks } from 'dateformat';
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: this,
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Get total count of friends
+UserSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// Get total count of friends
+UserSchema.virtual('thoughtCount').get(function () {
+  return this.thoughts.length;
+});
+
+// Create User model using UserSchema
+const User = model('User', UserSchema);
+
+module.exports = User;
